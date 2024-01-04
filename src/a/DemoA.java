a55fa63d1b8626a67862fca8823cf41fde73f40c
vanessawilliams <vanessawilliams@localhost>
2024-01-03 21:30:24 -0500

    implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -1,5 +1,18 @@
 package a;
 
 public class DemoA {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
